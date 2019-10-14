2a048503591bdfe77877855e63efe8ba0a8af8d7
Alex P <alex.pantec@gmail.com>
2019-10-14 23:56:12 +0300

    fix layout

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -1,12 +1,12 @@
 <html lang="en">
 <head>
     <meta charset="utf-8"/>
-    <title>Web Page Analyzer @hasSection('title')) - @yield('title') @endif</title>
+    <title> @hasSection('title'))@yield('title')  - @endif Web Page Analyzer</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#"></a>
+    <a class="navbar-brand" href="#">Web Page Analyzer</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
