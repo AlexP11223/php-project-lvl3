843c9b55b38b30fc8e001600a6d4a58573ccb65d
Alex P <alex.pantec@gmail.com>
2019-10-22 20:19:13 +0300

    add artisan commands

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -80,6 +80,8 @@ $app->singleton(
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
+$app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
+
 if (env('APP_DEBUG')) {
     $app->register(Barryvdh\Debugbar\LumenServiceProvider::class);
     $app->configure('debugbar');
