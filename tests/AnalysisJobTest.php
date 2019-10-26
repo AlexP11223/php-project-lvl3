aaee6333120323aea8e929308f2880a55b4e15d8
Alex P <alex.pantec@gmail.com>
2019-10-26 15:52:22 +0300

    add test

--- tests/AnalysisJobTest.php
+++ tests/AnalysisJobTest.php
@@ -20,6 +20,18 @@ class AnalysisJobTest extends TestCase
         $this->url = factory(Url::class, 1)->create()[0];
     }
 
+    public function testNormal()
+    {
+        $this->guzzler->queueResponse(new Response(200, ['content-length' => 6], 'hello!'));
+        (new AnalysisJob($this->url))->handle();
+
+        $this->url->refresh();
+        self::assertEquals(Url::SUCCEEDED, $this->url->state);
+        self::assertEquals(200, $this->url->statusCode);
+        self::assertEquals('hello!', $this->url->body);
+        self::assertEquals(6, $this->url->contentLength);
+    }
+
     public function testMissingContentLength()
     {
         $this->guzzler->queueResponse(new Response(200, [], 'hello!'));
